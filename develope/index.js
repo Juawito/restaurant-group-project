7116fd0d8c6cd64fbfcff70e7af8639e6132114f
Juawito <santiagojuan311@gmail.com>
2021-04-12 19:57:26 -0400

    created basic routes for server

--- develope/index.js
+++ develope/index.js
@@ -4,7 +4,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+const reservations = [];
+const waitlist = [];
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+app.get('/view-tables', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+app.get('/reservation', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+app.get('/api/tables', (req, res) => res.json(reservations));
+app.get('/api/waitlist', (req, res) => res.json(waitlist));
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
