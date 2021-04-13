82e8ae0bbefa881913ef9e0532a96e8b2bc81b83
Juawito <santiagojuan311@gmail.com>
2021-04-12 20:08:50 -0400

    finished get routes for server

--- develope/index.js
+++ develope/index.js
@@ -13,8 +13,26 @@ const waitlist = [];
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 app.get('/view-tables', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
-app.get('/reservation', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+app.get('/reservation', (req, res) => res.sendFile(path.join(__dirnamne, 'reserve.html')));
 app.get('/api/tables', (req, res) => res.json(reservations));
+app.get('/api/tables/:reservations', (req, res) => {
+    const chosen = req.params.reservations;
+    for (let index = 0; index < reservations.length; index++) {
+        if( chosen === reservations[index].routeName) {
+            return res.json(reservations[index])
+        };
+    } 
+    return res.json(false);
+})
 app.get('/api/waitlist', (req, res) => res.json(waitlist));
+app.get('/api/waitlist/:waitlist', (req, res) => {
+    const chosen = req.params.waitlist;
+    for (let index = 0; index < waitlist.length; index++) {
+        if( chosen === waitlist[index].routeName) {
+            return res.json(waitlist[index])
+        };
+    } 
+    return res.json(false);
+})
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
