a095ee9842cdf5c242b5988a29caa7ef40906737
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-04-12 21:10:37 -0400

    added javascript functions to the form

--- develope/script.js
+++ develope/script.js
@@ -1,12 +1,44 @@
+// Reservation Page
+$(".submit").on("click", function(event) {
+    event.preventDefault();
+})
+var newReservation = {
+    reserveName: $("#reserveName"),
+    reserevePhone: $("#reservePhone"),
+    reserveEmail: $("#reservemail"),
+    reserveNickname: $("#reserveNickname"),
+};
+
+console.log (newReservation)
+
+if (data) {
+    alert("You're booked!");
+}
+else {
+    alert ("Sorry, we're booked. You're on the waitlist");
+}
+$("#reserveName").val("");
+$("#reservePhone").val("");
+$("#reservemail").val("");
+$("#reserveNickname").val("");
+
+});
+
+// View Page
+
 var reservationDetails = document.getElementsByClassName("reservationDetails")
+
+var tableList = $("#tableList");
+
+var listItem = $("<li class='reservationDetails'>");
 
 reservationDetails.append (
         $("h3").text("Table" + (i+1)),
         $("hr"),
-        $("li").text("ID:" + tableData[i].id),
-        $("li").text("Name:" + tableData[i].name),
-        $("li").text("Email:" + tableData[i].email),
-        $("li").text("Phone:" + tableData[i].phone),
+        $("li").text("ID:" + tableData[i].reserveNickname),
+        $("li").text("Name:" + tableData[i].reserveName),
+        $("li").text("Email:" + tableData[i].reserveEmail),
+        $("li").text("Phone:" + tableData[i].reservePhone),
 
 )
 
